cf091dd14da150f60e02edb844f1761c5900e04c
Oleg Kupavtsev <olegkupavtsev@gmail.com>
2021-09-02 17:34:27 +0300

    40-50

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
+from django.template import loader
 
 from django.http import HttpResponse
 
 from .models import Bb
 
+# This is First version I
+# def index(request):
+#     s = 'Список объявлений\r\n\r\n\r\n'
+#     for bb in Bb.objects.order_by('-published'):
+#         s += bb.title + '\r\n' +  bb.content + '\r\n\r\n'
+#     return HttpResponse(s, content_type='text/plain; charset=utf-8')
+
+# This is Second version II
+# def index(request):
+#     template = loader.get_template('bboard/index.html')
+#     bbs = Bb.objects.order_by('-published')
+#     context = {'bbs': bbs}
+#     return HttpResponse(template.render(context, request))
+
+# This is Third version III
 def index(request):
-    s = 'Список объявлений\r\n\r\n\r\n'
-    for bb in Bb.objects.order_by('-published'):
-        s += bb.title + '\r\n' +  bb.content + '\r\n\r\n'
-    return HttpResponse(s, content_type='text/plain; charset=utf-8')
+    bbs = Bb.objects.order_by('-published')
+    return render(request, 'bboard/index.html', {'bbs': bbs})
