c461dda98ec6312828270ee86985a2d384c22529
Oleg Kupavtsev <olegkupavtsev@gmail.com>
2021-09-09 20:58:46 +0300

    60-70 Base

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
-from django.template import loader
+from django.views.generic.edit import CreateView
+from django.shortcuts   import render
+from django.template    import loader
+from django.urls        import reverse_lazy
 
-from django.http import HttpResponse
+from django.http        import HttpResponse
 
-from .models import Bb, Rubric
+from .models            import Bb, Rubric
+from .forms             import BbForm
 
 # This is First version I
 # def index(request):
@@ -32,3 +35,15 @@ def by_rubric(request, rubric_id):
     current_rubric  = Rubric.objects.get(pk=rubric_id)
     context         = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
     return render(request, 'bboard/by_rubric.html', context)
+
+
+class BbCreateView(CreateView):
+    template_name   = 'bboard/create.html'
+    form_class      = BbForm
+    success_url     = reverse_lazy('index')
+    # success_url     = '/bboard/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
