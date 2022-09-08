84cfac9be0aa1ecf18165a714370796623c553c6
Oleg Kupavtsev <olegkupavtsev@gmail.com>
2022-09-08 08:40:06 +0300

    pass reset

--- samplesite/samplesite/urls.py
+++ samplesite/samplesite/urls.py
@@ -17,7 +17,9 @@ from django.conf        import settings
 from django.conf.urls   import url
 from django.urls        import path, include
 from django.contrib     import admin
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (LoginView, LogoutView,
+                                       PasswordChangeDoneView, PasswordChangeView,
+                                       PasswordResetView, PasswordResetDoneView)
 
 
 # from bboard.views       import index
@@ -25,8 +27,18 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('bboard/', include('bboard.urls')),
     path('admin/', admin.site.urls),
+
     path('accounts/login/', LoginView.as_view(), name='login'),
     path('accounts/logout/', LogoutView.as_view(next_page='index'), name='logout'),
+    
+    path('accounts/password_change/', PasswordChangeView.as_view(template_name='registration/password_change_form.html'), name='password_change'),
+    path('accounts/password_change/done/', PasswordChangeDoneView.as_view(template_name='registration/password_changed.html'), name='password_change_done'),
+    
+    # path('accounts/password_reset/', PasswordResetView.as_view(
+    #                         template_name='registration/reset_password.html',
+    #                         subject_template_name='registration/reset_subject.txt',
+    #                         email_template='registration/reset_email.txt'
+    #                         ), name='password_reset'),
 ]
 
 if settings.DEBUG:
