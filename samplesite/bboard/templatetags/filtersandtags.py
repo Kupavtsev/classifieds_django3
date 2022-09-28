fe04ba5b6f5e6ec2fa4a1645717336d62b5e8d27
Oleg Kupavtsev <olegkupavtsev@gmail.com>
2022-09-28 21:10:55 +0300

    19 tags

--- samplesite/bboard/templatetags/filtersandtags.py
+++ samplesite/bboard/templatetags/filtersandtags.py
@@ -1,12 +1,23 @@
-from locale import currency
 from django import template
+from django.utils.safestring import mark_safe   # to add some html tags
 
 register = template.Library()
 
 
+# 1
 # @register.filter(name='cur')            
-@register.filter()            # this filter will be allowed in template
+@register.filter(is_safe=True)            # this filter will be allowed in template
 def currency(value, name='руб.'):
-    return f'{value} {name}'
+    return mark_safe(f'{value} <strong>{name}</strong>')
 
 # register.filter('currency', currency)     # The same as decorator
+
+# 2
+@register.simple_tag
+def lst(sep, *args):
+    return mark_safe(f'{sep.join(args)} (Итого <strong>{len(args)}</strong>)')
+
+# 3
+@register.inclusion_tag('tags/ulist.html')
+def ulist(*args):
+    return {'items': args}
