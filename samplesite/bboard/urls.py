3483a80b478c4e59969f893059978f1334e52a89
Oleg Kupavtsev <olegkupavtsev@gmail.com>
2022-10-12 19:05:12 +0300

    email, oauth need

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -1,44 +1,44 @@
 from django.urls import path
 from .models            import Bb
 
-from .views import (index, BbIndexView,
-                   by_rubric, BbByRubricView, BbByRubricViewListView,
-                   BbCreateView, BbAddFormView,
-                   BbDetailView, BbUpdateView, edit, BbDeleteView,
-                   BbMonthArchiveView, BbYearArchiveView, BbDayArchiveView, BbDayDetailView,
-                   rubrics, bbs)
+from .emails_handler import mail_send
+from . import views
+
 
 app_name = 'bboard'
 
 urlpatterns = [
 
+    # 8 Emails
+    path('email_send/', mail_send, name='mail_send'),
+
     # 7 Edit/Validation in forms
-    path('bbs/<int:rubric_id>', bbs, name='bbs'),
-    path('rubrics/', rubrics, name='rubrics'),
+    path('bbs/<int:rubric_id>', views.bbs, name='bbs'),
+    path('rubrics/', views.rubrics, name='rubrics'),
     
     # 6 DATES
-    path('<int:year>/<int:month>/<int:day>', BbDayArchiveView.as_view(model=Bb, date_field='published', month_format='%m')),
-    # path('<int:year>/', BbYearArchiveView.as_view()),
-    path('<int:year>/<int:month>', BbMonthArchiveView.as_view()),
+    path('<int:year>/<int:month>/<int:day>', views.BbDayArchiveView.as_view(model=Bb, date_field='published', month_format='%m')),
+    # path('<int:year>/', views.BbYearArchiveView.as_view()),
+    path('<int:year>/<int:month>', views.BbMonthArchiveView.as_view()),
 
     
     # 5 EDIT AD FORM - CLASS
-    path('edit/<int:pk>/', BbUpdateView.as_view(), name='edit'),
-    # path('edit/<int:pk>/', edit, name='edit'),
-    path('delete/<int:pk>/', BbDeleteView.as_view(), name='delete'),
+    path('edit/<int:pk>/', views.BbUpdateView.as_view(), name='edit'),
+    # path('edit/<int:pk>/', views.edit, name='edit'),
+    path('delete/<int:pk>/', views.BbDeleteView.as_view(), name='delete'),
 
     # 4 Add new Advertisment
-    path('add/', BbAddFormView.as_view(), name='add'),
+    path('add/', views.BbAddFormView.as_view(), name='add'),
 
     # 3 DETAIL VIEW OF EACH PRODUCT
-    path('detail/<int:pk>/', BbDetailView.as_view(), name='detail'),
-    # path('detail/<int:year>/<int:month>/<int:day>/<int:pk>/', BbDayDetailView.as_view(), name='detail'),
+    path('detail/<int:pk>/', views.BbDetailView.as_view(), name='detail'),
+    # path('detail/<int:year>/<int:month>/<int:day>/<int:pk>/', views.BbDayDetailView.as_view(), name='detail'),
 
     # 2 BY RUBRIC - FUNC
-    # path('<int:rubric_id>/', by_rubric, name='by_rubric'),
-    path('<int:rubric_id>/', BbByRubricView.as_view(), name='by_rubric'),
+    # path('<int:rubric_id>/', views.by_rubric, name='by_rubric'),
+    path('<int:rubric_id>/', views.BbByRubricView.as_view(), name='by_rubric'),
 
     # 1 MAIN PAGE
-    # path('', BbIndexView.as_view(), name='index'),
-    path('', index, name='index'),
+    # path('', views.BbIndexView.as_view(), name='index'),
+    path('', views.index, name='index'),
 ]
--- samplesite/samplesite/settings.py
+++ samplesite/samplesite/settings.py
@@ -186,3 +186,11 @@ BOOTSTRAP4 = {
     'success_css_class': 'has-success',
     'error_css_class': 'has-error',
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = env.str('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
